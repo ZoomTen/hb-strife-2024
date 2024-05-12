bc93d245205f28e36b02cd44d316bef1da8c7f4e
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-05-12 21:17:39 +0700

    hide navbar on android

--- android/src/org/zumi/strife30/NativeLoader.java
+++ android/src/org/zumi/strife30/NativeLoader.java
@@ -1,9 +1,19 @@
 package org.zumi.strife30;
+import android.os.Bundle;
+import android.view.View;
 
 public class NativeLoader extends android.app.NativeActivity
 {
     static
     {
         System.loadLibrary("main");
+    }
+
+    @Override
+    protected void onCreate(Bundle saved)
+    {
+        super.onCreate(saved);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
